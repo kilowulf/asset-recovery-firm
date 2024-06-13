842582a1b5c63b8f67fd0b734c4156d7d9157d7b
Aaron A. Brown <kilonow1275@hotmail.com>
2024-06-12 23:10:59 -0600

    sectioned about and contact page; styled

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,34 @@
 import React from "react";
+import handshakePhoto from "../img/pexelspaveldanilyuk8112180.jpg";
 
 const About = () => {
   return (
-    <section id="about" className="min-h-screen bg-gray-200">
-      <div className="container mx-auto px-4 py-20">
-        <h2 className="text-4xl font-bold text-center mb-10">About Overage Recovery</h2>
-        <p>
-          Asset Recovery Firm specializes in recovering overages funds of
-          residential properties that have fallen into foreclosure. We help
-          prior owners and occupiers recover funds called overages, which are
-          the excess funds collected when a foreclosed property is sold for more
-          than what is owed. State and county laws provide a specified time
-          window to recover these funds, and we are experts in navigating this
-          process.
-        </p>
+    <section id="about" className="min-h-screen bg-gray-100 border-2 mb-5">
+      <div className="container mx-auto px-4 pt-20">
+        <h2 className="text-4xl font-bold text-center mb-14">
+          About Overage Recovery
+        </h2>
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="md:w-1/2 p-8">
+            <p className="text-lg leading-relaxed max-w-xl">
+              Asset Recovery Firm specializes in recovering overage funds of
+              residential properties that have fallen into foreclosure. We help
+              prior owners and occupiers recover funds called overages, which
+              are the excess funds collected when a foreclosed property is sold
+              for more than what is owed. State and county laws provide a
+              specified time window to recover these funds, and we are experts
+              in navigating this process.
+            </p>
+          </div>
+          <div className="md:w-1/2 border-2 border-gray-300 p-2 rounded-lg max-w-xl">
+            <img
+              src={handshakePhoto}
+              alt="Overage Recovery"
+              className="rounded shadow-md"
+              style={{ width: "100%", height: "100%", maxWidth: "600px" }}
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,15 +1,32 @@
 import React from "react";
+import depressedGuy from "../img/pexels-andrew-3132388.jpg";
 
 const Services = () => {
   return (
     <section id="services" className="min-h-screen bg-gray-300">
       <div className="container mx-auto px-4 py-20">
         <h2 className="text-4xl font-bold text-center mb-10">Our Services</h2>
-        <ul>
-          <li>Recovering overage funds</li>
-          <li>Legal assistance for overage claims</li>
-          <li>Consultation and guidance</li>
-        </ul>
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="md:w-1/2 border-2 border-white-300 p-2 rounded-lg max-w-xl">
+            <img
+              src={depressedGuy}
+              alt="Overage Recovery"
+              className="rounded shadow-md"
+              style={{ width: "100%", height: "100%", maxWidth: "600px" }}
+            />
+          </div>
+          <div className="md:w-1/2 ml-9">
+            <p className="text-lg leading-relaxed">
+              Asset Recovery Firm specializes in recovering overage funds of
+              residential properties that have fallen into foreclosure. We help
+              prior owners and occupiers recover funds called overages, which
+              are the excess funds collected when a foreclosed property is sold
+              for more than what is owed. State and county laws provide a
+              specified time window to recover these funds, and we are experts
+              in navigating this process.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
