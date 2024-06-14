39b16c6ff5e8bc68b3047a595ff0f94591c0b062
Aaron A. Brown <kilonow1275@hotmail.com>
2024-06-13 21:28:27 -0600

    added further styling to about, contact, services

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,64 +23,84 @@ const Contact = () => {
   };
 
   return (
-    <section id="contact" className="min-h-screen bg-gray-400">
-      <div className="container mx-auto px-4 py-20">
-        <h2 className="text-4xl font-bold text-center mb-10">Contact Us</h2>
-        <form onSubmit={handleSubmit} className="max-w-lg mx-auto space-y-4">
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Name"
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Email"
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            placeholder="Message"
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            placeholder="Phone"
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            placeholder="Address (optional)"
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <input
-            type="text"
-            name="zip"
-            value={formData.zip}
-            onChange={handleChange}
-            placeholder="Zip Code (optional)"
-            className="w-full p-2 border border-gray-300 rounded"
-          />
-          <button
-            type="submit"
-            className="w-full p-2 bg-maroon text-white rounded hover:bg-red-700"
+    <section id="contact" className="min-h-screen bg-gray-100">
+      <div className="container mx-auto py-20 shadow-xl">
+        <div className="bg-maroon text-white rounded-lg py-4 mb-9 border border-gray-300 shadow-lg filter drop-shadow-md">
+          <h2 className="text-4xl font-bold text-center">Contact Us</h2>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:space-x-6">
+          <form
+            onSubmit={handleSubmit}
+            className="lg:w-1/2 bg-white p-8 rounded-lg border border-gray-300 shadow-xl mb-6 lg:mb-0"
           >
-            Submit
-          </button>
-        </form>
+            <div className="space-y-4">
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                placeholder="Name"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Email"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+              <textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                placeholder="Message"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+              <input
+                type="text"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                placeholder="Phone"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+              <input
+                type="text"
+                name="address"
+                value={formData.address}
+                onChange={handleChange}
+                placeholder="Address (optional)"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+              <input
+                type="text"
+                name="zip"
+                value={formData.zip}
+                onChange={handleChange}
+                placeholder="Zip Code (optional)"
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+              <button
+                type="submit"
+                className="w-full p-2 bg-maroon text-white rounded hover:bg-red-700"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+          <div className="lg:w-1/2 bg-white p-8 rounded-lg border border-gray-300 shadow-xl">
+            <h2 className="text-2xl font-bold mb-4">
+              Don't Miss Your Opportunity
+            </h2>
+            <p className="text-lg leading-relaxed">
+              Did you know that over 14 billion dollars from foreclosures, tax
+              overpayments, mortgage overpayments, funds related to property
+              sales, and money from other types of transactions are being held
+              by the U.S. Government – just waiting to be claimed!
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/Services.js
+++ src/components/Services.js
@@ -3,20 +3,20 @@ import depressedGuy from "../img/pexels-andrew-3132388.jpg";
 
 const Services = () => {
   return (
-    <section id="services" className="min-h-screen bg-gray-300">
-      <div className="container mx-auto px-4 py-20">
+    <section id="services" className="min-h-screen bg-gray-300 shadow-xl">
+      <div className="container mx-auto px-4 py-20 ">
         <h2 className="text-4xl font-bold text-center mb-10">Our Services</h2>
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="md:w-1/2 border-2 border-white-300 p-2 rounded-lg max-w-xl">
+        <div className="flex flex-col pb-2 md:flex-row items-center border-2 border-gray-300 rounded-lg shadow-xl">
+          <div className="md:w-1/2 border-2 border-gray-300 p-2 rounded-lg max-w-xl">
             <img
               src={depressedGuy}
               alt="Overage Recovery"
-              className="rounded shadow-md"
+              className="rounded shadow-xl"
               style={{ width: "100%", height: "100%", maxWidth: "600px" }}
             />
           </div>
-          <div className="md:w-1/2 ml-9">
-            <p className="text-lg leading-relaxed">
+          <div className="md:w-1/2 ml-9 ">
+            <p className="text-lg leading-relaxed m-4">
               Asset Recovery Firm specializes in recovering overage funds of
               residential properties that have fallen into foreclosure. We help
               prior owners and occupiers recover funds called overages, which
@@ -25,6 +25,46 @@ const Services = () => {
               specified time window to recover these funds, and we are experts
               in navigating this process.
             </p>
+          </div>
+        </div>
+        <div className="mt-10 border-2 border-gray-300 p-6 rounded-lg shadow-xl">
+          <h2 className="text-4xl font-bold text-center mb-10 pt-6">
+            The Process
+          </h2>
+
+          <div className="justify-center mb-10 text-8 text-center p-6">
+            <p className="text-lg leading-relaxed">It's Easy To Get Started</p>
+            <p className="text-lg leading-relaxed mb-9">
+              Below are the steps to get your claim going:
+            </p>
+            <div className="justify-center align-items-center">
+              <strong className="text-2xl">Step 1:</strong>
+              <p className="text-lg leading-relaxed mb-8 italic">
+                Contact us by calling, emailing, or completing the contact form.
+                Before we get the process started, we set up an initial call to
+                answer questions or concerns with no obligation to move forward.
+                We will also determine what funds are available to you.
+              </p>
+              <strong className="text-2xl">Step 2:</strong>
+              <p className="text-lg leading-relaxed mb-8 italic">
+                We will send you the necessary paperwork needed to work on
+                recovering your money.
+              </p>
+              <strong className="text-2xl">Step 3:</strong>
+              <p className="text-lg leading-relaxed mb-8 italic">
+                We will coordinate to have the paperwork signed and notarized
+                where needed.
+              </p>
+              <strong className="text-2xl">Step 4:</strong>
+              <p className="text-lg leading-relaxed mb-8 italic">
+                Return the paperwork back to us via mail or email.
+              </p>
+              <strong className="text-2xl">Step 5:</strong>
+              <p className="text-lg leading-relaxed mb-8 italic">
+                We take care of the rest! You sit back and let us do the work.
+                We will contact you as soon as a check is ready and on its way!
+              </p>
+            </div>
           </div>
         </div>
       </div>
